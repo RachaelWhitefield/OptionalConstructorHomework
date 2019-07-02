af07db6e4048e64e0ae82829dafe97bb3a0c2803
Fabulous_Rachael <rachael.whitefield@gmail.com>
2019-07-02 13:44:18 -0600

    working on constructors for new words

--- Word.js
+++ Word.js
@@ -5,3 +5,32 @@
 // a function that takes a character as an argument and calls the guess function on each letter object
 
 var letter = require("./Letter.js");
+
+function Word(word) {
+    var splitword = word.split("");
+    var wordArray = [];
+    splitword.forEach(function(element){
+        var letter = new letter(element);
+        wordArray.push(letter);
+    })
+    this.stringWord = word;
+    this.word = wordArray;
+    this.wordDisplay = function() {
+        var display = [];
+        this.word.forEach(function(element){
+            display.push(element.display());
+        })
+        return display;
+    }
+    this.guess = function(letter) {
+        var found = false;
+        this.word.forEach(function(element) {
+            if(element.check(letter)) {
+                found = true;
+            }
+        })
+        return found;
+    }
+}
+
+module.exports = Word;
