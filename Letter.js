1e3442e1a572d196e6b94136e88c592d79f47ac0
Fabulous_Rachael <rachael.whitefield@gmail.com>
2019-07-02 13:47:27 -0600

    added letter js pseudocode

--- Letter.js
+++ Letter.js
@@ -7,3 +7,22 @@
 
 var inquirer = require("inquirer");
 
+function Letter(letter) {
+    this.letter = letter;
+    this.guessed = false;
+    this.display = function() {
+        if(this.guessed === false) {
+            return "_";
+        } else {
+            return this.letter;
+        }
+    }
+    this.check = function(value) {
+        if(value === this.letter) {
+            this.guessed = true;
+            return true;
+        }
+    }
+}
+
+module.exports = Letter;
