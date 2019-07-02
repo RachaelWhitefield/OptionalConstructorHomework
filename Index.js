4d7314d3fedce8aa3cba0ecb3a4a8a7d12f37c11
Fabulous_Rachael <rachael.whitefield@gmail.com>
2019-07-01 18:41:38 -0600

    deleted package-lock and node modules

--- Index.js
+++ Index.js
@@ -1,7 +1,9 @@
 // contains the logic for the game
 // randomly selects a word and uses the "Word" constructor to store it
 // prompts the user for each guess and keeps track of the users remaining guesses
+// SHOULD I LET THE USER KNOW THIS IS CHESS THEMED???
 
 var inquirer = require(inquirer);
 var Word = require("./Word.js");
 var randomWords = require("./List.js");
+
--- List.js
+++ List.js
@@ -1,3 +1,5 @@
 var listOfWords = [
     "chess", "checkmate", "queen", "king", "knight", "bishop", "rook", "pawn", "rank", "file"
 ];
+
+// Exports the list of words to the other scripts
